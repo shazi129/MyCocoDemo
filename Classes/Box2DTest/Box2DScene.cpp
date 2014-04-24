f2b3a7ce12db90d7188ffaf46638b97f7c3da710
zhangwen <i.am.zw129@gmail.com>
2014-04-24 20:53:41 +0800

    add commit

--- Classes/Box2DTest/Box2DScene.cpp
+++ Classes/Box2DTest/Box2DScene.cpp
@@ -54,26 +54,27 @@ bool Box2DScene::init()
     pMenu->setPosition(CCPointZero);
     this->addChild(pMenu, 1);
 
-    CCSprite * ball = CCSprite::create("CloseNormal.png");
+    b2BodyDef bodyDef;
+
+    CCSprite * ball = CCSprite::create("CloseNormal.png"); //小球对应的精灵
     ball->setAnchorPoint(ccp(0.5, 0.5));
     ball->setPosition(VisibleRect::center());
     this->addChild(ball);
 
-    b2BodyDef bodyDef;
-    bodyDef.type = b2_dynamicBody;
-    bodyDef.userData = ball;
-    bodyDef.linearDamping = 0.0f;
-    bodyDef.position.Set(ball->getPositionX() * PTM_RATIO, ball->getPositionY()* PTM_RATIO);
-    mBallBody = mWorld->CreateBody(&bodyDef);
+    bodyDef.type = b2_dynamicBody; //小球是可以动的
+    bodyDef.userData = ball;    //这只是为了保存数据，可以通过物体找到对应的精灵
+    bodyDef.linearDamping = 0.0f; //初速度为0
+    bodyDef.position.Set(ball->getPositionX() * PTM_RATIO, ball->getPositionY()* PTM_RATIO); //初始位置
+    mBallBody = mWorld->CreateBody(&bodyDef); // 在Box2D世界中创建一个物体
 
     b2PolygonShape bodyShape;
-    bodyShape.SetAsBox(20* PTM_RATIO, 20* PTM_RATIO);
+    bodyShape.SetAsBox(20* PTM_RATIO, 20* PTM_RATIO); //小球的大小和形状~ 方形的？？？圆形的应该是b2CircleShape
 
-    b2FixtureDef bodyFixture;
-    bodyFixture.density = 1;
-    bodyFixture.shape = &bodyShape;
-    bodyFixture.friction = 0.1f;
-    bodyFixture.restitution = 1.0f;
+    b2FixtureDef bodyFixture; //小球的定制器
+    bodyFixture.density = 1;  // 密度
+    bodyFixture.shape = &bodyShape; //形状
+    bodyFixture.friction = 0.1f; //摩擦力，因为掉落过程中没有物体间的摩擦，所以没用
+    bodyFixture.restitution = 1.0f; //弹力，100%反弹~~~
     mBallBody->CreateFixture(&bodyFixture);
 
     this->scheduleUpdate();
@@ -98,22 +99,21 @@ void Box2DScene::update(float delta)
 void Box2DScene::initBox2D()
 {
     // init world
-    b2Vec2 gravity;
-    gravity.Set(0.0f, -10.0f);
-    mWorld = new b2World(gravity);
+    b2Vec2 gravity; 
+    gravity.Set(0.0f, -10.0f); //一个向下10单位的向量，作为重力减速度，Box2D中默认的单位是秒和米
+    mWorld = new b2World(gravity); // 创建一个有重力加速度的世界
 
-    b2BodyDef bodyDef;
-    bodyDef.type = b2_staticBody;
-    b2PolygonShape borderShape;
-    b2FixtureDef borderFixture;
-    borderFixture.density = 1;
+    b2BodyDef bodyDef;   //一个物体的定义
+    bodyDef.type = b2_staticBody; //物体的类型，不动的，坚硬的~ 可以把这样的物体当成地面
+    b2PolygonShape borderShape; // 一个方形的形状
+    b2FixtureDef borderFixture; // 一个定制器
 
-    //地板
-    bodyDef.position.Set(0, 0);
-    borderShape.SetAsBox(VisibleRect::right().x * PTM_RATIO, 0);
+    //定义地板
+    bodyDef.position.Set(0, 0); // 地板在0，0位置
+    borderShape.SetAsBox(VisibleRect::right().x * PTM_RATIO, 0); //设置长度和高度，这里描述的地板就像是X轴，是一条线
     borderFixture.shape = &borderShape;
-    b2Body * bottomBorder = mWorld->CreateBody(&bodyDef);
-    bottomBorder->CreateFixture(&borderFixture);
+    b2Body * bottomBorder = mWorld->CreateBody(&bodyDef); //在世界中创建一个物体（地面）
+    bottomBorder->CreateFixture(&borderFixture);          //定制地面的形状
 
 }
 
